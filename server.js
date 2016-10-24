0b49de457f1342e2b164924f65428e8c93876c79
maimous@gmail.com <stefaleon@gmail.com>
2016-10-24 12:28:53 +0300

    initialize repo

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+const PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('private route hit!');
+		next();	
+	},
+	logger: function(req, res, next){
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	} 
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+	res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){
+	console.log('server started on port ' + PORT);
+});
+
